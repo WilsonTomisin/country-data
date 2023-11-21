d90c00156c3a87bcd368a5e30370d5806152ed5e
Wilson Tomisin <tommywilson972@gmail.com>
2023-11-21 15:25:26 +0100

    responsive search component

--- src/components/SearchedCountry.jsx
+++ src/components/SearchedCountry.jsx
@@ -13,19 +13,19 @@ export const SearchedCountry = ({isDark}) => {
     const darktheme = isDark ? ' bg-slate-900 text-white' : 'bg-slate-100 text-slate-950'
     const darktheme2 = isDark ? ' bg-slate-800 text-white' : 'bg-white text-slate-950'
     return (
-    <div className={`${darktheme} duration-700 transition-colors`}>
-        <button onClick={()=> navigate('/')}
-        className={`${darktheme2} ml-12 mt-12 rounded-lg shadow-md shadow-slate-400 px-6 py-2 font-semibold flex justify-between`}>
-            <span className=' font-extrabold text-xl'> <BsArrowLeft/> </span>
-            Back
-        </button>
-      <div className=' flex items-center justify-between p-12'>
-        <div className='  mt-12 w-1/2 h-auto pr-10'>
-            <img src={flags.png} alt={name+' flag'} className=' w-full '/>
+        <div className={`${darktheme} duration-700 transition-colors`}>
+            <button onClick={()=> navigate('/')}
+            className={` ${darktheme2} ml-12 mt-12 rounded-lg shadow-md shadow-slate-400 px-6 py-2 font-semibold flex justify-between items-center`}>
+                <span className=' font-extrabold text-xl'> <BsArrowLeft/> </span>
+                Back
+            </button>
+            <div className=' flex flex-col md:flex-row items-start md:items-center justify-between p-6 md:p-12'>
+        <div className='  mt-12 w-full md:w-1/2 h-auto pr-10'>
+            <img src={flags.png} alt={name+' flag'} className=' w-full'/>
         </div>
         <div className=' mt-12 w-1/2 h-auto'>
-            <h1 className='  text-3xl mb-5 font-bold'>{name}</h1>
-            <div className=' grid grid-rows-5 grid-flow-col gap-4'>
+            <h1 className='  text-3xl mb-5 font-bold '>{name}</h1>
+            <div className=' grid grid-cols-1 md:grid-rows-5 md:grid-flow-col gap-4 '>
                 <p className=' font-semibold'>
                     Native name: <span className=' text-slate-600 font-medium'> {nativeName}</span>
                 </p>
@@ -55,20 +55,20 @@ export const SearchedCountry = ({isDark}) => {
             </div>
             <div className=' mt-11'>
                 <span className=' font-semibold'>Border countries:</span>
-                <div className=' grid grid-rows-3 grid-flow-col gap-2'>
+                <div className=' grid grid-cols-1 md:grid-flow-col gap-2'>
                     { borders ? borders.map((item,index)=>(
                         
-                        <span key={index} className=' mr-2 px-6 shadow-lg shadow-slate-500 py-2 rounded-lg font-semibold '>
+                        <span key={index} className=' mr-2 px-6 shadow-lg py-2 rounded-lg font-semibold '>
                             {item}
                         </span>
                     
-                )) : <span> no border country</span>}
+                )) : <span> no bordering country</span>}
                 </div>
                 
             </div>
         </div>
-      </div>
-    </div>
+            </div>
+        </div>
     
     
   )
